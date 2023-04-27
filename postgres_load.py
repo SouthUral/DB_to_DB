5b949819b081c1b0b8d716a277482d89a84d7f6e
vladimir <vladimir1012kov@gmail.com>
2023-04-27 12:44:40 +0300

    Задана структура работы скрипта

--- postgres_load.py
+++ postgres_load.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 from contextlib import contextmanager
+from log_pack import log_error, log_success
+
 
 class pg_context:
     @staticmethod
@@ -21,24 +23,53 @@ class pg_context:
         log_success('Connection to postgresql database is closed')
 
 
-class Postgres:
-    def __init__(self, ext_pg_conf: PgConfig, save_pg_conf: PgConfig):
+class Postgres(pg_context):
+    def __init__(self, ext_pg_conf: PgConfig, save_pg_conf: PgConfig, chunk_extract=500, chunk_save=20):
         self.ext_pg_conf = ext_pg_conf
         self.save_pg_conf = save_pg_conf
+        self.chunk_extract = chunk_extract
+        self.chunk_save = chunk_save
 
-    def work_Data(self):
+    def _worker(self):
+        with self.conn_context(self.save_pg_conf(), cursor_factory=DictCursor) as save_conn, self.conn_context(self.ext_pg_conf(), cursor_factory=DictCursor) as ext_conn:
+            Saver = PostgresSaver(save_conn, self.chunk_save)
+            Extractor = PostgresExtrator(ext_conn, self.chunk_extract)
+            Extractor.check_id = Saver.chek_created_id()
+            for data in Extractor.generator():
+                Saver.data_recorder(data)
+            log_success('All data is recorded')
 
 
+class PostgresExtrator():
+    check_id: str
 
+    def __init__(self, pg_conn, chunk: int):
+        self.pg_conn = pg_conn
+        self.cursor = pg_conn.cursor()
+        self.chunk = chunk
 
-class PostgresExtractor(pg_context):
-    def __init__(self):
+    def generator_data(self):
         pass
 
 
-class PostgresSaver(pg_context):
-    def __init__(self):
+class PostgresSaver():
+    def __init__(self, pg_conn, chunk: int):
+        self.pg_conn = pg_conn
+        self.cursor = pg_conn.cursor()
+        self.chunk = chunk
+    
+    def data_recorder(self, data):
         pass
+
+    def chek_created_id(self):
+        self.cursor.execute('''
+            SELECT m.created_id
+            FROM device.messages m
+            ORDER BY m.created_id DESC
+            LIMIT 1;'''
+            )
+        return self.cursor.fetchone()
+
 
 
 class PgConfig:
@@ -48,5 +79,8 @@ class PgConfig:
         self.password = os.getenv(password)
         self.host = os.getenv(host)
         self.port = os.getenv(port)
+
+    def __call__(self):
+        return self.__dict__
 
 
