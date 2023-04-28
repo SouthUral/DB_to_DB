32605364f2c9248efb1d7c2525ffadfce787f8f1
vladimir <vladimir1012kov@gmail.com>
2023-04-28 12:29:13 +0300

    Реализуется первая проверка

--- postgres_load.py
+++ postgres_load.py
@@ -1,4 +1,5 @@
 import os
+import json
 import psycopg2
 from dotenv import load_dotenv
 from psycopg2.extensions import connection as _connection
@@ -24,6 +25,10 @@ class pg_context:
 
 
 class Postgres(pg_context):
+    count_partition: int
+    Saver: PostgresSaver
+    Extractor: PostgresExtrator
+
     def __init__(self, ext_pg_conf: PgConfig, save_pg_conf: PgConfig, chunk_extract=500, chunk_save=20):
         self.ext_pg_conf = ext_pg_conf
         self.save_pg_conf = save_pg_conf
@@ -31,16 +36,33 @@ class Postgres(pg_context):
         self.chunk_save = chunk_save
 
     def _worker(self):
+        '''Управляющий блок запускающий логику'''
         with self.conn_context(self.save_pg_conf(), cursor_factory=DictCursor) as save_conn, self.conn_context(self.ext_pg_conf(), cursor_factory=DictCursor) as ext_conn:
-            Saver = PostgresSaver(save_conn, self.chunk_save)
-            Extractor = PostgresExtrator(ext_conn, self.chunk_extract)
-            Extractor.check_id = Saver.chek_created_id()
+            self.Saver = PostgresSaver(save_conn, self.chunk_save)
+            self.Extractor = PostgresExtrator(ext_conn, self.chunk_extract)
+            # Запуск блока проверки
+            if not self._check_db_partitions() and self.count_partition == 0:
+
+
+
+            self.Extractor.check_id = Saver.chek_created_id()
             for data in Extractor.generator():
                 Saver.data_recorder(data)
             log_success('All data is recorded')
 
+    def _check_db_partitions(self) -> bool:
+        '''Блок проверки'''
+        self.data_for_partition = self.Extractor.get_distinct_object()
+        self.counter_partiton = self.Saver.get_counter_partiton()
+        return len(self.data_for_partition) == self.counter_partiton
+
+
+
+
+
 
 class PostgresExtrator():
+    '''Класс для взаимодействия с БД откуда считываются данные'''
     check_id: str
 
     def __init__(self, pg_conn, chunk: int):
@@ -51,13 +73,54 @@ class PostgresExtrator():
     def generator_data(self):
         pass
 
+    def get_distinct_object(self):
+        '''Возвращает список с уникальным object_id'''
+        self.cursor.execute('''
+            SELECT DISTINCT ON (object_id)
+            created_id,
+            device_id,
+            object_id,
+            mes_id,
+            mes_time,
+            mes_code,
+            mes_status,
+            mes_data,
+            event_value,
+            event_data
+            FROM device.messages;'''
+            )
+        return self.cursor.fetchall()
+
 
 class PostgresSaver():
+    '''Класс для взаимодействия с БД куда производится запись'''
     def __init__(self, pg_conn, chunk: int):
         self.pg_conn = pg_conn
         self.cursor = pg_conn.cursor()
         self.chunk = chunk
+
+    def get_counter_partiton(self):
+        '''Возвращает количество секций в таблице'''
+        self.cursor.execute('''
+            SELECT COUNT(table_name) 
+            FROM information_schema.tables
+            WHERE table_name LIKE 'message\_%';'''
+            )
+        return self.cursor.fetchone()
+
+    def make_partition(self, data):
+        '''Вызывает процедуру, которая разбивает таблицу на секции'''
+        self.cursor.execute("CALL device.check_section($1, $2);", json.dumps(data), None)
+        self.clean_data()
+        self.pg_conn.commit()
+        log_success('Sections have been created in the table device.messages')
+
+    def clean_data(self):
+        '''Очищает таблицу device.messages вместе с секциями'''
+        self.cursor.execute("TRUNCATE TABLE device.messages")
+        log_success('Table device.messages is cleared')
     
+
     def data_recorder(self, data):
         pass
 
